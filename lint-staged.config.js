9c5b040d2e49cc6812414a470ee74588b9cd4f3d
Alex Unku <alex.unku@miceanddice.com>
2021-11-11 23:45:04 +0000

    uploading gh-pages branch

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    '*.{js,jsx}': ['eslint --fix'],
-    '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': ['prettier --write'],
-    '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged']
-};
